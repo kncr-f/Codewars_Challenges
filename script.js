19825b82792d4974448f29834b284be181f864c2
Fatih Kincir <kincirfatih@gmail.com>
2022-05-07 23:04:12 +0200

    6kyu_ Multiples of 3 or 5

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ function findOdd(A) {
 // Solution 2
 
 const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+// 6kyu Multiples of 3 or 5
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.Additionally, if the number is negative, return 0(for languages that do have them).
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// Solution 1
+
+function solution(num) {
+    let myArr = [];
+    for (let i = 0; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            myArr.push(i)
+
+        }
+    }
+
+    const sum = myArr.reduce((a, b) => a + b, 0)
+    return sum
+}
+
+// Solution 2
+
+function solution(number) {
+    var sum = 0;
+
+    for (var i = 1; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
+    }
+    return sum;
+}
