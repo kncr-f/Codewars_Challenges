1dc460bb8e0f44a2e976f28deb7c5001ab0c118a
Fatih Kincir <kincirfatih@gmail.com>
2022-05-13 20:32:18 +0200

    6kyu Find The Parity Outlier

--- script.js
+++ script.js
@@ -255,3 +255,53 @@ function createPhoneNumber2(numbers) {
 
     return format;
 }
+
+// 6kyu Find The Parity Outlier
+
+// You are given an array(which will have a length of at least 3, but could be very large) containing integers.The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.Write a method that takes the array as an argument and returns this "outlier" N.
+
+//     Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11(the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160(the only even number)
+
+// Solution 1
+
+function findOutlier(int) {
+    var even = int.filter(a => a % 2 == 0);
+    var odd = int.filter(a => a % 2 !== 0);
+    return even.length == 1 ? even[0] : odd[0];
+}
+
+// Solution 2
+
+function findOutlier2(integers) {
+    let countEven = 0;
+    let countOdd = 0;
+    for (let i = 0; i < integers.length; i++) {
+        if (integers[i] % 2 === 0) {
+            countEven++
+        } else {
+            countOdd++
+        }
+    }
+    if (countEven === 1) {
+        for (let j = 0; j < integers.length; j++) {
+            if (integers[j] % 2 === 0) {
+                return integers[j]
+            }
+        }
+    }
+
+    if (countOdd === 1) {
+        for (let j = 0; j < integers.length; j++) {
+            if (integers[j] % 2 === 1) {
+                return integers[j]
+            }
+        }
+    }
+
+
+}
