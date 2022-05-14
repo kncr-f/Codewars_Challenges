16d910501499f23fe20b766268f9c87327c7c5fb
Fatih Kincir <kincirfatih@gmail.com>
2022-05-14 21:05:35 +0200

    6kyu Duplicate Encoder

--- script.js
+++ script.js
@@ -342,3 +342,30 @@ function duplicateCount(text) {
     let result = [...new Set(duplicate)]
     return result.length
 }
+
+//6kyu Duplicate Encoder
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string.Ignore capitalization when determining if a character is a duplicate.
+
+//     Examples
+// "din"      => "((("
+// "recede"   => "()()()"
+// "Success"  => ")())())"
+// "(( @"     => "))((" 
+
+// Solution
+
+function duplicateEncode(word) {
+    let myArr = word.toLowerCase().split("")
+    let duplicate = myArr.filter((item, index) => myArr.indexOf(item) != index);
+    let result = [...new Set(duplicate)]
+
+    for (let i = 0; i < myArr.length; i++) {
+        if (result.includes(myArr[i])) {
+            myArr[i] = ")"
+        } else {
+            myArr[i] = "("
+        }
+    }
+    return myArr.join("")
+}
