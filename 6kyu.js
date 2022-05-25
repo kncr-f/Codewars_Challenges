bf311d0231ad5c9f688ded8e6a7d7ab7b188159f
Fatih Kincir <kincirfatih@gmail.com>
2022-05-25 11:50:25 +0200

    6kyu Playing with digits

--- 6kyu.js
+++ 6kyu.js
@@ -626,3 +626,30 @@ function uniqueInOrder(arg) {
     return myArr.filter((v, i, a) => a[i] !== a[i + 1])
 
 }
+
+//6kyu Playing with digits
+
+// Some numbers have funny properties.For example:
+// 89 -- > 8¹ + 9² = 89 * 1
+// 695 -- > 6² + 9³ + 5⁴= 1390 = 695 * 2
+// 46288 -- > 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+// In other words:
+// Is there an integer k such as : (a ^ p + b ^ (p + 1) + c ^ (p + 2) + d ^ (p + 3) + ...) = n * k
+// If it is the case we will return k, if not return -1.
+
+// Solution
+
+function digPow(n, p) {
+    let arr = Array.from(n.toString()).map(Number);
+    let res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        res += Math.pow(arr[i], p)
+        p++
+    }
+    if (Number.isInteger(res / n)) {
+        return res / n
+    }
+    return -1
+}
