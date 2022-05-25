ad6322a1f87d4827a4c7e6af938b1e6966a42c8d
Fatih Kincir <kincirfatih@gmail.com>
2022-05-25 10:44:59 +0200

    6kyu Unique In Order

--- 6kyu.js
+++ 6kyu.js
@@ -587,3 +587,42 @@ decodeMorse = function (morseCode) {
 
 
 }
+
+//6kyu Unique In Order
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD') == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1, 2, 2, 3, 3]) == [1, 2, 3]
+
+// Solution 1
+function uniqueInOrder(arg) {
+    if (Array.isArray(arg)) {
+        var myArr = arg
+    } else if (typeof arg === "string") {
+        myArr = arg.split("");
+    }
+    let res = [];
+    for (let i = 0; i < myArr.length; i++) {
+        if (myArr[i] !== myArr[i + 1]) {
+            res.push(myArr[i])
+        }
+    }
+    return res
+}
+
+// Solution 2
+
+function uniqueInOrder(arg) {
+    if (Array.isArray(arg)) {
+        var myArr = arg
+    } else if (typeof arg === "string") {
+        myArr = arg.split("");
+    }
+
+    return myArr.filter((v, i, a) => a[i] !== a[i + 1])
+
+}
