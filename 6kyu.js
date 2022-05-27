fb29d873fdca85d609f4a1b0b2f803461c6d5551
Fatih Kincir <kincirfatih@gmail.com>
2022-05-27 08:59:20 +0200

    Does my number look big in this

--- 6kyu.js
+++ 6kyu.js
@@ -717,4 +717,33 @@ function toCamelCase(str) {
     return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
 }
 
+// 6kyu Does my number look big in this?
+
+// A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base.In this Kata, we will restrict ourselves to decimal(base 10).
+
+// For example, take 153(3 digits), which is narcisstic:
+
+// 1 ^ 3 + 5 ^ 3 + 3 ^ 3 = 1 + 125 + 27 = 153
+// and 1652(4 digits), which isn't:
+
+// 1 ^ 4 + 6 ^ 4 + 5 ^ 4 + 2 ^ 4 = 1 + 1296 + 625 + 16 = 1938
+
+// Solution
+
+function narcissistic(n) {
+    let myArr = Array.from(n.toString()).map(Number);
+    let sum = 0;
+    for (digit of myArr) {
+        sum += Math.pow(digit, myArr.length)
+    }
+
+    if (sum === n) {
+        return true
+    }
+    return false
+}
+
+
+
+
 
