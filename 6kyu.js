8eb648912745711038e9233c7acbb337615ed534
Fatih Kincir <kincirfatih@gmail.com>
2022-05-27 23:40:48 +0200

    6kyu Find the missing letter

--- 6kyu.js
+++ 6kyu.js
@@ -780,3 +780,35 @@ function findUniq(arr) {
 }
 
 
+// 6kyu Find the missing letter
+
+// #Find the missing letter
+
+// Write a method that takes an array of consecutive(increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array.And it will be always exactly one letter be missing.The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a', 'b', 'c', 'd', 'f'] -> 'e'['O', 'Q', 'R', 'S'] -> 'P'
+
+// ["a", "b", "c", "d", "f"] -> "e"
+// ["O", "Q", "R", "S"] -> "P"
+//         (Use the English alphabet with 26 letters!)
+
+// Solution
+function findMissingLetter(array) {
+    let alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+    const first = array[0]
+    const firstItem = alphabets.indexOf(first)
+    const myArr = alphabets.splice(firstItem, array.length + 1)
+
+    for (let i = 0; i < myArr.length; i++) {
+        if (array.indexOf(myArr[i]) == -1) {
+            return myArr[i]
+
+        }
+    }
+}
+
