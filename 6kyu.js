704170e533948789ffd1332fcdcf73d33a83adb9
Fatih Kincir <kincirfatih@gmail.com>
2022-05-27 11:15:13 +0200

    6kyu find the unique number

--- 6kyu.js
+++ 6kyu.js
@@ -757,6 +757,26 @@ function isPangram(str) {
     return alphabets.every(item => lower.includes(item))
 }
 
+// 6kyu Find the unique number
 
+// There is an array with some numbers.All numbers are equal except for one.Try to find it!
+
+// findUniq([1, 1, 1, 2, 1, 1]) === 2
+// findUniq([0, 0, 0.55, 0, 0]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// Solution
+function findUniq(arr) {
+    const myArr = arr.sort((a, b) => a - b)
+    for (let i = 0; i < myArr.length; i++) {
+        if (myArr[i] === myArr[i + 1]) {
+            return myArr[myArr.length - 1]
+        } else {
+            return myArr.shift()
+        }
+    }
+}
 
 
