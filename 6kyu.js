2b7adc6a32d31a4bd7eb71cf73473c74da5b70cc
Fatih Kincir <kincirfatih@gmail.com>
2022-05-28 22:48:17 +0200

    6kyu split strings

--- 6kyu.js
+++ 6kyu.js
@@ -812,3 +812,28 @@ function findMissingLetter(array) {
     }
 }
 
+//6kyu Split strings
+
+// Complete the solution so that it splits the string into pairs of two characters.If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore('_').
+
+//     Examples:
+
+// * 'abc' => ['ab', 'c_']
+//     * 'abcdef' => ['ab', 'cd', 'ef']
+
+//Solution
+function solution(str) {
+    const myArr = str.split("");
+    console.log(myArr)
+    if (myArr.length % 2 === 1) {
+        myArr.push("_")
+    }
+    let res = []
+    for (let i = 0; i < myArr.length; i += 2) {
+
+        res.push(myArr[i] + myArr[i + 1])
+    }
+    return res
+}
+
+
