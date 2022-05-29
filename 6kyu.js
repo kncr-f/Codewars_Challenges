78af47ae3c66a09318f198d91b4e03d37da0495c
Fatih Kincir <kincirfatih@gmail.com>
2022-05-29 23:02:40 +0200

    6kyu build tower

--- 6kyu.js
+++ 6kyu.js
@@ -856,5 +856,36 @@ function sortArray(arr) {
 }
 
 
+// 6kyu Build Tower
 
+// Build a pyramid - shaped tower given a positive integer number of floors.A tower block is represented with "*" character.
 
+// For example, a tower with 3 floors looks like this:
+
+// [
+//     "  *  ",
+//     " *** ",
+//     "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//     "     *     ",
+//     "    ***    ",
+//     "   *****   ",
+//     "  *******  ",
+//     " ********* ",
+//     "***********"
+// ]
+
+// Solution
+function towerBuilder(n) {
+    var tower = [];
+    for (var i = 0; i < n; i++) {
+        tower.push(" ".repeat(n - i - 1)
+            + "*".repeat((i * 2) + 1)
+            + " ".repeat(n - i - 1))
+    }
+
+    return tower;
+}
