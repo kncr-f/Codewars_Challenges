ff09941afd09f524d9f14c461a48de373491a8a2
Fatih Kincir <kincirfatih@gmail.com>
2022-05-31 15:25:36 +0200

    6kyu consecutive strings

--- 6kyu.js
+++ 6kyu.js
@@ -1090,3 +1090,43 @@ function isPrime(num) {
     }
     return num > 1;
 }
+
+// 6kyu Consecutive strings
+
+// You are given an array(list) strarr of strings and an integer k.Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+//     Examples:
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling(length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// Solution
+
+function longestConsec(strarr, k) {
+    const n = strarr.length;
+    if (n === 0 || k > n || k <= 0) {
+        return ""
+    }
+    let longString = "";
+    let newString = "";
+
+    for (let i = 0; i < strarr.length; i++) {
+        newString = strarr.slice(i, i + k);
+        if (newString.join('').length > longString.length) {
+            longString = newString.join('');
+        }
+    }
+
+    return longString
+
+}
