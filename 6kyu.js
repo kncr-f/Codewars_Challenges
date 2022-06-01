d6c4c74d7f9b5cbc80c78d0cbb751d287d44150f
Fatih Kincir <kincirfatih@gmail.com>
2022-06-01 20:02:31 +0200

    6kyu The Supermarket Queue

--- 6kyu.js
+++ 6kyu.js
@@ -1280,3 +1280,42 @@ function bouncingBall(h, b, w = 1.5) {
     }
     return result - 1
 }
+
+// 6kyu The Supermarket Queue
+
+// There is a queue for the self - checkout tills at the supermarket.Your task is write a function to calculate the total time required for all the customers to check out!
+
+// input
+// customers: an array of positive integers representing the queue.Each integer represents a customer, and its value is the amount of time they require to check out.
+//     n: a positive integer, the number of checkout tills.
+//         output
+// The function should return an integer, the total time required.
+
+// Solution
+
+function queueTime(arr, n) {
+    if (arr.length === 0) {
+        return 0
+    }
+    if (n == 1) {
+        return arr.reduce((a, b) => a + b, 0);
+    }
+
+    let myArr = arr.splice(0, n)
+    let maxMyArr = myArr.reduce((a, b) => { return Math.max(a, b) });
+    let sumRest = arr.reduce((a, b) => a + b, 0)
+
+    if (maxMyArr >= sumRest) {
+        return maxMyArr
+    }
+
+    for (let j = 0; j < arr.length; j++) {
+
+        let minMyArr = myArr.reduce((a, b) => { return Math.min(a, b) });
+        myArr[myArr.indexOf(minMyArr)] += arr[j]
+
+    }
+
+    return myArr.reduce((a, b) => { return Math.max(a, b) });
+
+}
