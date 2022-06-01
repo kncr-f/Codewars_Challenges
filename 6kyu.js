829087a3b541afcaee2dd2e3ca75d316890593fc
Fatih Kincir <kincirfatih@gmail.com>
2022-06-01 23:15:08 +0200

    6kyu Mexican Wave

--- 6kyu.js
+++ 6kyu.js
@@ -1319,3 +1319,30 @@ function queueTime(arr, n) {
     return myArr.reduce((a, b) => { return Math.max(a, b) });
 
 }
+
+
+// 6kyu Mexican Wave
+
+// Task
+// In this simple Kata your task is to create a function that turns a string into a Mexican Wave.You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+//     Rules
+// 1.  The input string will always be lower case but maybe empty.
+
+//  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+// Example
+// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+// Solution
+
+function wave(str) {
+    let arr = []
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i]
+        if (letter === " ") {
+            continue
+        } else {
+            arr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1))
+        }
+    }
+    return arr
+}
