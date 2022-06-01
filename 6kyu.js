4329fa643342de41b4c0fd30b036c7d6758b980a
Fatih Kincir <kincirfatih@gmail.com>
2022-06-01 13:03:53 +0200

    6kyu Bouncing Ball

--- 6kyu.js
+++ 6kyu.js
@@ -1248,3 +1248,35 @@ function inArray2(arr1, arr2) {
 function inArray3(arr1, arr2) {
     return arr1.filter(arr1Item => arr2.find(arr2Item => arr2Item.match(arr1Item)))
 }
+
+
+// 6kyu Bouncing Ball
+// A child is playing with a ball on the nth floor of a tall building.The height of this floor, h, is known.
+
+// He drops the ball out of the window.The ball bounces(for example), to two - thirds of its height(a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window(including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Solution
+function bouncingBall(h, b, w = 1.5) {
+    let max = 2 * h
+    let result = 0
+    if (h <= 0 || b <= 0 || b >= 1 || w >= h) {
+        return -1
+    }
+    for (let i = 0; i < max; i++) {
+        if (h > w) {
+            result += 2
+        }
+        h = (h * b)
+    }
+    return result - 1
+}
