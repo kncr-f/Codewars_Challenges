480aae039f6d1ce7f9597a1e6df4a74711fcea1e
Fatih Kincir <kincirfatih@gmail.com>
2022-06-02 23:21:02 +0200

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eurekagit add .

--- 6kyu.js
+++ 6kyu.js
@@ -1363,6 +1363,8 @@ function wave2(str) {
 }
 wave2("hello")
 
+
+
 // 6kyu Break camelCase
 
 // Complete the solution so that the function will break up camel casing, using a space between words.
@@ -1383,4 +1385,48 @@ function solution(str) {
     }
 
     return str.replace(/([A-Z])/g, " $1");
+}
+
+
+
+//6kyu Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+
+
+// The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata.What's the use of saying "Eureka"? Because this sum gives the same number.
+
+// In effect: 89 = 8 ^ 1 + 9 ^ 2
+
+// The next number in having this property is 135.
+
+// See this property again: 135 = 1 ^ 1 + 3 ^ 2 + 5 ^ 3
+
+// We need a function to collect these numbers, that may receive two integers a, b that defines the range[a, b](inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+//     Let's see some cases:
+
+// sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+
+
+// Solution
+function sumDigPow(a, b) {
+    let arr = []
+    for (let i = a; i <= b; i++) {
+        let bb = i.toString()
+        let aa = i.toString().length;
+        let sum = 0;
+        if (aa == 1) {
+            arr.push(i)
+        } else {
+            for (let j = 0; j < aa; j++) {
+
+                sum += Math.pow(bb[j], j + 1)
+                if (sum == i) {
+                    arr.push(i)
+                }
+            }
+        }
+    }
+    return arr
 }
