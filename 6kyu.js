f65abf975316afd9c357c467136429d55edcade3
Fatih Kincir <kincirfatih@gmail.com>
2022-06-03 23:42:30 +0200

    Count characters in your string

--- 6kyu.js
+++ 6kyu.js
@@ -1430,3 +1430,17 @@ function sumDigPow(a, b) {
     }
     return arr
 }
+
+// 6kyu Count characters in your string
+
+// The main idea is to count all the occurring characters in a string.If you have a string like aba, then the result should be { 'a': 2, 'b': 1 }.
+
+// What if the string is empty ? Then the result should be empty object literal, {}.
+
+// Solution
+
+function count(string) {
+    let obj = {}
+    string.split("").map(x => obj[x] = obj[x] + 1 || 1)
+    return obj
+}
