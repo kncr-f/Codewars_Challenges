d87d96e2248e31375dcd332d7c58f293fd5abfad
Fatih Kincir <kincirfatih@gmail.com>
2022-06-04 13:00:18 +0200

    4kyu Remove Zeros

--- 4kyu.js
+++ 4kyu.js
@@ -28,3 +28,35 @@ function snail(array) {
 
     return finalArr
 }
+
+
+// 4kyu Remove Zeros
+
+// Write a function that takes an array of values and moves all elements that are zero to the end of the array, otherwise preserving the order of the array.The zero elements must also maintain the order in which they occurred.
+
+// For example, the following array
+
+// [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
+
+// is transformed into
+
+// [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+
+// Zero elements are defined by either 0 or "0".Some tests may include elements that are not number literals.
+
+You are NOT allowed to use any temporary arrays or objects.You are also not allowed to use any Array.prototype or Object.prototype methods.
+
+// Solution
+
+function removeZeros(array) {
+    for (let j = 0; j < array.length - 1; j++) {
+        for (let i = 0; i < array.length - 1; i++) {
+            if ((array[i] === 0 || array[i] === "0") && (array[i + 1] !== 0 && array[i + 1] !== "0")) {
+                let aa = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aa
+            }
+        }
+    }
+    return array;
+}
