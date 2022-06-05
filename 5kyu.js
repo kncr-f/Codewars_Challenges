f1744105e72956bc568d2f7cee907b462b840ec5
Fatih Kincir <kincirfatih@gmail.com>
2022-06-05 22:12:34 +0200

    5kyuRGB To Hex Conversion

--- 5kyu.js
+++ 5kyu.js
@@ -221,3 +221,33 @@ function dirReduc(arr) {
     }
     return result
 }
+
+// 5kyu RGB To Hex Conversion
+
+// The rgb function is incomplete.Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+//     Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0, 0, 0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+// Solution
+
+function componentToHex(c) {
+    var hex;
+    if (c > 255) {
+        return hex = "FF"
+    } else if (c < 0) {
+        return hex = "00"
+    }
+    hex = c.toString(16).toUpperCase();
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgb(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
