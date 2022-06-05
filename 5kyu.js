e90ff5a06cb9c2f1d929aaa8b179b1e492e6afee
Fatih Kincir <kincirfatih@gmail.com>
2022-06-05 13:51:39 +0200

    Where my anagrams at?

--- 5kyu.js
+++ 5kyu.js
@@ -122,3 +122,44 @@ function humanReadable(s) {
 }
 
 
+// 5kyu Where my anagrams at?
+
+// What is an anagram ? Well, two words are anagrams of each other if they both contain the same letters.For example:
+
+// 'abba' & 'baab' == true
+
+// 'abba' & 'bbaa' == true
+
+// 'abba' & 'abbba' == false
+
+// 'abba' & 'abca' == false
+// Write a function that will find all the anagrams of a word from a list.You will be given two inputs a word and an array with words.You should return an array of all the anagrams or an empty array if there are none.For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+// anagrams('laser', ['lazing', 'lazy', 'lacer']) => []
+
+// Solution
+
+const areAnagram = (str1, str2) => str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('');
+function anagrams(str, arr) {
+    let anagrams = []
+    arr.forEach(item => {
+        let word = ""
+        for (let i = 0; i < item.length; i++) {
+
+            if (str.includes(item[i])) {
+                word += item[i]
+            }
+        }
+        console.log(word)
+        if ((areAnagram(str, word) && item === word)) {
+            anagrams.push(word)
+        }
+
+    })
+
+    return anagrams
+}
