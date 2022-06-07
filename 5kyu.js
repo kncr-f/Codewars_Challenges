6abc3fb221759d2fc408796f051a44f00b592615
Fatih Kincir <kincirfatih@gmail.com>
2022-06-07 10:01:09 +0200

    5kyu Poduct of consecutive Fib numbers

--- 5kyu.js
+++ 5kyu.js
@@ -348,3 +348,59 @@ function maxSequence(arr) {
 
 }
 
+
+// 5kyu Product of consecutive Fib numbers
+
+// The Fibonacci numbers are the numbers in the following integer sequence(Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+//     F(n) = F(n - 1) + F(n - 2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod(for product), we search two Fibonacci numbers F(n) and F(n + 1) verifying
+
+// F(n) * F(n + 1) = prod.
+
+// Your function productFib takes an integer(prod) and returns an array:
+
+// [F(n), F(n + 1), true] or { F(n), F(n + 1), 1 } or(F(n), F(n + 1), True)
+// depending on the language if F(n) * F(n + 1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n + 1), false] or { F(n), F(n + 1), 0 } or(F(n), F(n + 1), False)
+// F(n) being the smallest one such as F(n) * F(n + 1) > prod.
+
+
+
+// Solution
+
+function productFib(prod) {
+    function fib(num) {
+        let arr = []
+        for (let i = 0; i <= num; i++) {
+            if (i == 0 || i == 1) {
+                arr[i] = i
+            } else {
+                arr[i] = arr[i - 1] + arr[i - 2]
+            }
+        }
+
+        return arr[arr.length - 1]
+    }
+
+    let n = 0;
+    while (fib(n) * fib(n + 1) < prod) {
+        n++
+    }
+
+    if (fib(n) * fib(n + 1) == prod) {
+        return [fib(n), fib(n + 1), true]
+    } else {
+        return [fib(n), fib(n + 1), false]
+    }
+
+}
+
