85c40e4b1f0dbceee0bf91f8791c8f9cce42219c
Fatih Kincir <kincirfatih@gmail.com>
2022-06-10 21:38:52 +0200

    5kyu String incrementer

--- 5kyu.js
+++ 5kyu.js
@@ -604,3 +604,39 @@ function generateHashtag(str) {
     }).join('');
     return r.length > 140 ? false : r;
 }
+
+
+// 5kyu String incrementer
+
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number.the number 1 should be appended to the new string.
+//     Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+// Solution
+
+function incrementString(str) {
+    const exceptLast = str.slice(0, -1);
+    const lastDigit = str.slice(-1).match(/[0-9]/);
+    console.log(lastDigit)
+    if (!lastDigit) {
+        return str + "1"
+    } else if (lastDigit != 9) {
+        return exceptLast + (+lastDigit + 1)
+    } else {
+        return incrementString(exceptLast) + "0"
+    }
+}
