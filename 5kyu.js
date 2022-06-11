f4ea00d90bfabb8d135e7ea54ce3a41792767d96
Fatih Kincir <kincirfatih@gmail.com>
2022-06-12 00:01:25 +0200

    5kyu Number of trailing zeros of N

--- 5kyu.js
+++ 5kyu.js
@@ -640,3 +640,28 @@ function incrementString(str) {
         return incrementString(exceptLast) + "0"
     }
 }
+
+// 5kyu
+
+// Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+//     N! = 1 * 2 * 3 * ... * N
+
+// Be careful 1000! has 2568 digits...
+
+// For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+// Examples
+// zeros(6) = 1
+// # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 -- > 1 trailing zero
+
+// zeros(12) = 2
+// # 12! = 479001600 -- > 2 trailing zeros
+
+// Solution
+
+function zeros(n) {
+    var result = 0;
+    while (n = Math.floor(n / 5)) result += n;
+    return result;
+}
