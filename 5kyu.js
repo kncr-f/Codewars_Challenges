08d170bf1b607d36305856d4054fdf2e3180c58c
Fatih Kincir <kincirfatih@gmail.com>
2022-06-14 23:28:09 +0200

    5kyu First non-repeating character cleverer solution added

--- 5kyu.js
+++ 5kyu.js
@@ -699,3 +699,13 @@ var firstNonRepeatingLetter = function (str) {
 
     return ""
 };
+
+// Solution 2
+
+function firstNonRepeatingLetter(s) {
+    var t = s.toLowerCase();
+    for (var x = 0; x < t.length; x++)
+        if (t.indexOf(t[x]) === t.lastIndexOf(t[x]))
+            return s[x];
+    return "";
+}
