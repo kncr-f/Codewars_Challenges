77032bfb6def705c175693549c12aa08141e4ca1
Fatih Kincir <kincirfatih@gmail.com>
2022-06-15 18:55:46 +0200

    5kyu Scramblies

--- 5kyu.js
+++ 5kyu.js
@@ -709,3 +709,73 @@ function firstNonRepeatingLetter(s) {
             return s[x];
     return "";
 }
+
+// 5kyu Scramblies
+
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+//     Notes:
+
+// Only lower case letters will be used(a - z).No punctuation or digits will be included.
+// Performance needs to be considered.
+//     Examples
+// scramble('rkqodlw', 'world') ==> True
+// scramble('cedewaraaossoqqyt', 'codewars') ==> True
+// scramble('katas', 'steak') ==> False
+
+// Solution1 
+
+function scramble(str1, str2) {
+
+    let score = 0;
+    let obj1 = {}
+    let obj2 = {}
+    for (let i = 0; i < str2.length; i++) {
+        if (obj2[str2[i]] === undefined) {
+            obj2[str2[i]] = 1
+        } else {
+            obj2[str2[i]] += 1
+        }
+    }
+    for (let i = 0; i < str1.length; i++) {
+        if (obj1[str1[i]] === undefined) {
+            obj1[str1[i]] = 1
+        } else {
+            obj1[str1[i]] += 1
+        }
+    }
+
+    for (const item in obj2) {
+        if (obj1[item] >= obj2[item]) {
+            score += obj2[item]
+        }
+    }
+
+    return score === str2.length ? true : false
+}
+
+// Solution 2 (couldn't pass time efficiency test but logic is true)
+
+
+function scramble(str1, str2) {
+
+    let score = 0;
+
+    for (const letter of str2) {
+        for (const letter2 of str1) {
+
+            if (letter === letter2) {
+                score++
+                str1 = str1.replace(letter2, "")
+                break
+            }
+        }
+
+    }
+
+    if (score == str2.length) {
+        return true
+    }
+    return false
+
+}
