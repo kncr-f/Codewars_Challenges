e004bc433b8a0ee22be82c839c29a8de118dee70
Fatih Kincir <kincirfatih@gmail.com>
2022-06-16 22:48:43 +0200

    5kyu Perimeter of squares in a rectangle

--- 5kyu.js
+++ 5kyu.js
@@ -851,3 +851,31 @@ function score(dice) {
     return score
 
 }
+
+// 5kyu Perimeter of squares in a rectangle
+
+// The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80
+// Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+// Hint:
+// See Fibonacci sequence
+
+// Ref:
+// http://oeis.org/A000045
+
+// The function perimeter has for parameter n where n + 1 is the number of squares(they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+//     perimeter(5)  should return 80
+// perimeter(7)  should return 216
+
+// Solution
+
+function perimeter(n) {
+    let fibonacci = [0, 1]
+    for (let i = 1; i <= n; i++) {
+        fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+    }
+
+    const total = fibonacci.reduce((a, b) => a + b, 0)
+    return 4 * total
+}
